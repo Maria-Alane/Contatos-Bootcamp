be122e757e8d4261bd021bced951e7d3451a8459
maria-alane <maria.alane.bs@gmail.com>
2022-01-25 11:50:19 -0300

    Implementando a classe viewholder

--- app/src/main/java/com/chaveirinho/contatosbootcamp/ContactsAdapter.kt
+++ app/src/main/java/com/chaveirinho/contatosbootcamp/ContactsAdapter.kt
@@ -1,13 +1,17 @@
 package com.chaveirinho.contatosbootcamp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactsAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactsAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
+
 
     override fun onBindViewHolder(holder: ContactsAdapter.ViewHolder, position: Int) {
         holder.bindItem(contactsList[position])
@@ -15,5 +19,18 @@ class ContactsAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapte
 
     override fun getItemCount(): Int {
         return contactsList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact) {
+
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_phone_number)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
+
     }
 }
